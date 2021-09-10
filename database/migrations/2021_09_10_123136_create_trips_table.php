1922b04a7938d6054fa94c7683fe05379713000a
shatyrbekova <perizat.shatyrbekova@gmail.com>
2021-09-10 15:04:50 +0200

    created table with properties

---
+++ database/migrations/2021_09_10_123136_create_trips_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTripsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trips', function (Blueprint $table) {
+            $table->id();
+            $table->text('city');
+            $table->text('hotel');
+            $table->integer('days');
+            $table->double('price', 8,2);
+            $table->text('description');
+            $table->tinyInteger(('discount'));
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trips');
+    }
+}
